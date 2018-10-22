3f5a9f09e1df37bc2b59bf6120a1299f74e102ff
agodot <zoldy.wf@gmail.com>
2018-10-22 14:01:42 -0600

    Improvements on note playing. Also fixed bug on automata logic.

--- cellular_music.py
+++ cellular_music.py
@@ -8,13 +8,13 @@ pygame.midi.init()
 
 screen = pygame.display.set_mode((1500, 800))
 
-box_x = 0
-box_y = 0
+box_x = 3
+box_y = 3
 
-grid_x = 12
-grid_y = 5
+grid_x = 18
+grid_y = 11
 
-block_size = 100
+block_size = 50
 
 grid = [[0 for y in range(grid_y)] for x in range(grid_x)]
 light_grid = [[0 for y in range(grid_y)] for x in range(grid_x)]
@@ -25,10 +25,10 @@ import time, sys, math
 i = pygame.midi.Input(1)
 p_clock = pygame.time.Clock()
 
-win_font = pygame.font.SysFont("Deja Vu", 20)
+win_font = pygame.font.SysFont("Deja Vu", 50)
 
 player = pygame.midi.Output(0)
-player.set_instrument(6)
+player.set_instrument(45)
 instrument = 0
 
 while True:
@@ -40,13 +40,14 @@ while True:
 
     for y in range(grid_y):
         for x in range(grid_x):
-            light_grid[x][y] = max(grid[x][y], light_grid[x][y], hit_grid[x][y])
+            light_grid[x][y] = grid[x][y] #max(grid[x][y], light_grid[x][y], hit_grid[x][y])
 
             lg = light_grid[x][y]
             c = (50*(1-lg**2), 20+(lg**2)*235, 20+90*(1-lg**2))
-            if hit_grid[x][y]: c = (150,255,60)
+            if hit_grid[x][y]: c = (255,255,255)
             pygame.draw.rect(screen, c, (100+ x*block_size, 100 + y*block_size, block_size-2, block_size-2) )
             light_grid[x][y] = 0 if light_grid[x][y]-0.2 < 0 else light_grid[x][y]-0.2
+
 
             # Now update the grid.
             neighbors = []
@@ -62,14 +63,17 @@ while True:
             neighbors.append(grid[x][grid_y-1 if y-1 < 0 else y-1])
 
             s = sum(neighbors)
+            screen.blit(win_font.render(str(s), 1, (0,0,0)), (100+x*block_size, 100+y*block_size))
 
-            if s < 3: new_offs.append((x,y))
+            if s < 2: new_offs.append((x,y))
             elif s > 3: new_offs.append((x,y))
-            if s == 3: new_ons.append((x,y))
+            if s == 3 and not grid[x][y]: new_ons.append((x,y))
+
+
 
     for (x,y) in new_offs:
         grid[x][y] = 0
-        player.note_off(x+12*y+36,127)
+        #player.note_off(x+12*y+36,127)
     for (x,y) in new_ons:
         player.note_on(x+12*y+36,127)
         grid[x][y] = 1
@@ -88,24 +92,26 @@ while True:
             if k>0:
                 if k == 127: continue
                 grid[(e.data1-36)%12 + box_x][int((e.data1-36)/12) + box_y] = 1
-                hit_grid[(e.data1-36)%12 + box_x][int((e.data1-36)/12) + box_y] = 1
+                #hit_grid[(e.data1-36)%12 + box_x][int((e.data1-36)/12) + box_y] = 1
 
                 sx = (e.data1-36)%12 + box_x
                 sy = int((e.data1-36)/12) + box_y
-                grid[sx][(sy+1) % grid_y] = 1
 
-                if sy - 1 > 0:
-                    grid[sx][sy-1] = 1
-                else:
-                    grid[sx][grid_y-1] = 1
-                grid[(sx+1) % grid_x][sy] = 1
+                sx_l = sx-1 if sx-1 >= 0 else grid_x -1
+                sx_r = (sx+1) % grid_x
 
-                if sx - 1 > 0 :
-                    grid[sx-1][sy] = 1
-                else:
-                    grid[grid_x-1][sy] = 1
-            else:
-                hit_grid[(e.data1-36)%12 + box_x][int((e.data1-36)/12) + box_y] = 0
+                sy_u = sy-1 if sy-1 >= 0 else grid_y -1
+                sy_d = (sy+1) % grid_y
+
+                #grid[sx_r][sy] = 1  # Right
+                #grid[sx_l][sy] = 1 # Left
+                grid[sx][sy_u] = 1  # Up
+                #grid[sx_l][sy_d] = 1 # Left down
+                grid[sx_r][sy_d] = 1 # Right down
+                #grid[sx][sy_d] = 1 # Down
+                grid[sx_l][sy_u] = 1 # Left up
+                #grid[sx_r][sy_u] = 1 # Right up
+
 
     pygame.display.flip()
-    p_clock.tick(30)
+    p_clock.tick(10)
